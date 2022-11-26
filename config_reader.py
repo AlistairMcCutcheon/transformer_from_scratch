92a38249da4e17bde3ae573768e1b96913b95b7f
Alistair McCutcheon <alistairdmccutcheon@gmail.com>
2022-11-26 17:47:10 +1100

    feat: Added ConfigReader to read a module and return it as an object

---
+++ config_reader.py
@@ -0,0 +1,16 @@
+import importlib.util
+import sys
+from pathlib import Path
+
+class ConfigReader:
+
+    @staticmethod
+    def read_config(path):
+        # Reads a path to a python module and returns the module as an object
+        # https://docs.python.org/3/library/importlib.html#importing-a-source-file-directly
+        module_name = Path(path).stem
+        spec = importlib.util.spec_from_file_location(Path(path).stem, path)
+        module = importlib.util.module_from_spec(spec)
+        sys.modules[module_name] = module
+        spec.loader.exec_module(module)
+        return module
