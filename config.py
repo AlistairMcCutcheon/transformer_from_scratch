7237ba5f95bad5b3972902b9098b42b66ae10033
Alistair McCutcheon <alistairdmccutcheon@gmail.com>
2022-11-26 17:49:53 +1100

    feat: Added config scafolding to train.py, and fixed a typo in the spelling of __main__

---
+++ config.py
@@ -0,0 +1,4 @@
+batch_size = 10
+
+def square(x):
+    return x**2
--- train.py
+++ train.py
@@ -1,7 +1,13 @@
+import argparse
+from config_reader import ConfigReader
 
 def main():
     pass
 
 
-if __name__ == "main":
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config_file_path", default="config.py")
+    args = parser.parse_args()
+    config = ConfigReader.read_config(args.config_file_path)
     main()
