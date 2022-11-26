d94fe3da9446a862b694adae12e5b0b89c0cc6e5
Alistair McCutcheon <alistairdmccutcheon@gmail.com>
2022-11-26 18:58:28 +1100

    refactor: added directory organisation

--- tools/train.py
+++ tools/train.py
@@ -1,5 +1,5 @@
 import argparse
-from config_reader import ConfigReader
+from utils.config_reader import ConfigReader
 
 def main(config):
     pass
@@ -7,7 +7,7 @@ def main(config):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--config_file_path", default="config.py")
+    parser.add_argument("--config_file_path", default="tools/config.py")
     args = parser.parse_args()
     config = ConfigReader.read_config(args.config_file_path)
     main(config)
